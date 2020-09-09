a06af73ff497dfa47375dae45bf52ba24fffb5b3
tennessejoyce <47008993+tennessejoyce@users.noreply.github.com>
2020-09-09 14:00:17 -0400

    Update hhg.py

--- hhg.py
+++ hhg.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.signal.windows import get_window
 from matplotlib.ticker import MultipleLocator,LogLocator
 from matplotlib.colors import LogNorm
+from scipy.interpolate import interp1d as interp
 
 def fourier_transform(dipole,dt,omega,max_harmonic=30,zero_padding_factor=10,window='blackman',t_phase=0,time_axis=0):
 	'''Handles the Fourier transform for HHG with all the extra subtleties.
@@ -44,15 +45,6 @@ def multiply_along_axis(a,b,axis=0):
 	return a*np.reshape(b,dim_array)
 
 
-def hhg_intensity_heatmap(dipole,intensties,interpolation_matrix,intensity_axis=1,**kwargs):
-	'''Example of how to create a heatmap showing HHG spectra as a function of intensity.
-	You can specify in the arguments which axis is intensity and which is time.'''
-	freq,spec = fourier_transform(dipole,**kwargs)
-
-
-
-
-
 
 def plot_hhg_spectrum(freq,spec):
 	'''Example plotting script for an HHG spectrum.'''
@@ -66,3 +58,77 @@ def plot_hhg_spectrum(freq,spec):
 	plt.tight_layout()
 	plt.savefig("hhgSpec/"+str(i)+".png",dpi=500)
 	plt.close()
+
+
+def magnitude_heatmap(x,y,z,ax,noise_threshold=0.2):
+	'''
+		Heatmap for the magnitude of the HHG spectrum.
+			'x' is either intensity or macroscopic angle (works for either)
+			'y' is the harmonic number ('freq')
+			'z' is the harmonic spectrum (complex amplitude)
+			'ax' is the axes on which the plot will be drawn.
+	'''
+	z = np.abs(z)**2
+	z = np.maximum(np.abs(z)/np.max(np.abs(z)),noise_threshold)
+	#Truncate below the noise threshold.
+	z = np.maximum(z,noise_threshold)
+	color_map = ax.contourf(y,x,np.transpose(z),levels=np.geomspace(noise_threshold,1,num_levels),norm=LogNorm(),cmap='jet')
+	ax.set_ylabel('harmonic order')
+	ticks = np.arange(0,20,0.5)
+	ticks_mask = (ticks>=x[0]-0.1)*(ticks<=x[-1]+0.1)
+	ticks = ticks[ticks_mask]
+	ax.yaxis.set_major_locator(MultipleLocator(1))
+	ax.yaxis.set_minor_locator(MultipleLocator(0.25))
+	ax.tick_params(axis='y',right=True,which='both')
+	plt.colorbar(color_map,ax=ax,ticks=[1.0,0.1,0.01,0.001])
+
+
+def phase_heatmap(x,y,z,ax,noise_threshold=0.2):
+	z_mask = np.abs(z)/np.max(np.abs(z)) < np.sqrt(noise_threshold)
+	phase = np.angle(z)
+	phase[z_mask] = np.nan
+	color_map = ax.contourf(y,x,np.transpose(phase),100,cmap='hsv')
+	color_map.get_cmap().set_bad(color='grey')
+	ax.set_ylabel('harmonic order')
+	ticks = np.arange(0,20,0.5)
+	ticks_mask = (ticks>=x[0]-0.1)*(ticks<=x[-1]+0.1)
+	ticks = ticks[ticks_mask]
+	ax.yaxis.set_major_locator(MultipleLocator(1))
+	ax.yaxis.set_minor_locator(MultipleLocator(0.25))
+	ax.tick_params(axis='y',right=True,which='both')
+	plt.colorbar(color_map,ax=ax,ticks=[-np.pi,-np.pi/2,0,np.pi/2,np.pi-1e-10])
+
+def delay_heatmap(x,y,z,ax,noise_threshold=0.2):
+	z_mask = np.abs(z)/np.max(np.abs(z)) < np.sqrt(noise_threshold)
+	delay = -np.gradient(np.unwrap(np.angle(z)),axis=1)/(x[1]-x[0])/(2*np.pi)
+	delay = np.maximum(np.minimum(delay,2),-2)
+	delay[z_mask] = np.nan#np.min(delay[1-z_mask])
+	color_map = ax.contourf(y,x,np.transpose(delay),100,cmap='jet')
+	color_map.get_cmap().set_bad(color='grey')
+	ax.set_ylabel('harmonic order')
+	ticks = np.arange(0,20,0.5)
+	ticks_mask = (ticks>=x[0]-0.1)*(ticks<=x[-1]+0.1)
+	ticks = ticks[ticks_mask]
+	#ax.set_xticks(ticks)
+	ax.yaxis.set_major_locator(MultipleLocator(1))
+	ax.yaxis.set_minor_locator(MultipleLocator(0.25))
+	ax.tick_params(axis='y',right=True,which='both')
+	plt.colorbar(color_map,ax=ax,ticks=range(-3,4))
+
+
+
+def macro_response(freq,spec,theta):
+	#Apply the transformation matrix to get H(omega,kappa)
+	hwk = tInt.dot(spec)
+	#Next, we change variables from kappa to theta by interpolating
+	#onto the desired values of theta provided in the argument.
+	macro_hhg = []
+	for hn,col in zip(freq,np.transpose(hwk)): #Iterate over frequencies.
+		#Response is an even function of r.
+		double_col = np.concatenate([col,col])
+		double_kappa = np.concatenate([kappa,-kappa])
+		#The values of theta correponding to kappa for this particular frequency.
+		sample_theta = np.arcsin(double_kappa/kappa_factor/hn)
+		#Do the interpolation.
+		macro_hhg.append(interp(sample_theta,double_col,kind='cubic',fill_value=0.0,bounds_error=False)(theta))
+	return np.transpose(np.array(macro_hhg))
